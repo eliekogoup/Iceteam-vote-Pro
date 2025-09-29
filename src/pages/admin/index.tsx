2afb0e996a1cf78753ec72a6d23a93f29ebfd8cf
eliekogoup <elieko@hotmail.fr>
2025-09-29 17:53:46 +0100

    Fix import path for supabaseClient in AdminPage

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { supabase } from "../lib/supabaseClient";
+import { supabase } from "../../lib/supabaseClient";
 
 export default function AdminPage() {
   const [editions, setEditions] = useState<any[]>([]);
