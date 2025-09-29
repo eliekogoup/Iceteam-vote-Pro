2a750a4c293a8d691f080cf7ef711cb119e461bd
eliekogoup <elieko@hotmail.fr>
2025-09-29 22:35:40 +0100

    Fix import paths in login.tsx

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { supabase } from "../../lib/supabaseClient";
+import { supabase } from "../lib/supabaseClient";
 import { useRouter } from "next/router";
 
 export default function LoginPage() {
@@ -48,3 +48,4 @@ export default function LoginPage() {
   );
 
 }
+
